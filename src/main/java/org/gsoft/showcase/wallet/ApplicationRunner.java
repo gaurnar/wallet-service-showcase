10b6428150ae9461423ce7684dca673facbb856b
Igor Popov <gaurnar@gmail.com>
2019-10-06 16:31:11 +0700

    main class for service, Gradle application plugin support, fat JAR generation

--- src/main/java/org/gsoft/showcase/wallet/ApplicationRunner.java
+++ src/main/java/org/gsoft/showcase/wallet/ApplicationRunner.java
@@ -1,8 +1,31 @@
 package org.gsoft.showcase.wallet;
 
+import java.net.InetSocketAddress;
+
 public class ApplicationRunner {
 
+    private static final int DEFAULT_PORT = 8080;
+
     public static void main(String[] args) {
-        // TODO
+        try {
+            int port = getPort(args);
+
+            Application application = new Application(new InetSocketAddress(port));
+            application.start();
+
+            System.out.println("Running wallet service at http://localhost:" + port + "/ ...");
+            System.out.println("Press Ctrl+C to exit");
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+    }
+
+    private static int getPort(String[] args) {
+        if (args.length >= 1) {
+            return Integer.parseInt(args[0]);
+        } else {
+            return DEFAULT_PORT;
+        }
     }
 }
